43b59d58621543908e09190ca5ce4d65668b2a52
Nick <hi@zookee.one>
2017-02-08 14:40:44 +0100

    added konami code

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,29 @@ resetColors = function() {
   
 };
 
+
+
+//check out https://github.com/zookee1/js-konami-code
+document.addEventListener("keydown", konamiCode, false);
+var konami_index = 0;
+
+function konamiCode(e) {
+    /*---
+     * ↑↑ ↓↓ ←→ ←→ b a
+     ---*/
+    var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var keyCode = e.keyCode;
+
+    if(keyCode === konami_keys[konami_index++]) {
+        if (konami_index === konami_keys.length) {
+        	window.location('http://www.savethechildren.org.uk/about-us/emergencies/syria-appeal')
+        }
+    } else {
+        konami_index = 0;
+    }
+
+}
+
 update = function() {
   
   timer = setTimeout( function() {
